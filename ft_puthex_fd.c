bfa09197957a3fe2a7d2168c9f3299cf3cdfc8f0
jmykkane <jmykkane@student.hive.fi>
2022-11-29 17:45:01 +0200

    put hex fix

--- ft_puthex_fd.c
+++ ft_puthex_fd.c
@@ -6,7 +6,7 @@
 /*   By: jmykkane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 16:51:27 by jmykkane          #+#    #+#             */
-/*   Updated: 2022/10/24 16:51:30 by jmykkane         ###   ########.fr       */
+/*   Updated: 2022/11/29 17:44:43 by jmykkane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,22 @@ void	ft_puthex_fd(unsigned long long num, int fd)
 	char	buf[17];
 
 	i = 0;
+	if (num == 0)
+		ft_putnbr_fd(0, fd);
 	while (num != 0)
 	{
 		temp = num % 16;
 		if (temp < 10)
 		{
 			c = temp + 48;
-			buf[i] = c;
+			buf[i++] = c;
 		}
 		else
 		{
 			c = temp + 87;
-			buf[i] = c;
+			buf[i++] = c;
 		}
 		num = num / 16;
-		i++;
 	}
 	while (--i >= 0)
 		ft_putchar_fd(buf[i], fd);
