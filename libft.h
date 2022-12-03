77f9aadd610839a3252a173257f7fa026f4d8be3
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2022-12-03 16:43:53 +0200

    added itoa hex

--- libft.h
+++ libft.h
@@ -50,6 +50,7 @@ char	*ft_strchr(const char *s, int c);
 int		ft_hexlen(unsigned long long num);
 char	*ft_strrchr(const char *s, int c);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa_hex(unsigned long long num);
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_lstadd_back(t_list **lst, t_list *new);
